c6a17e573f5b539150d97f6a2cbf88d8d41f432c
Sai Lyon Ho <sailyonho@Sais-MacBook-Air.local>
2021-10-06 09:16:57 -0400

    done with episode 196

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:shop_app/providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   const ProductDetailScreen({Key? key}) : super(key: key);
@@ -8,6 +11,14 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context)!.settings.arguments as String;
-    return Scaffold(appBar: AppBar());
+    final loadedProduct = Provider.of<Products>(
+      context,
+      listen: false,
+    ).findById(productId);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(loadedProduct.title),
+      ),
+    );
   }
 }
